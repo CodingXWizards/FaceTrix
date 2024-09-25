ab697d7f8368805db814d78e434588e07fba177f
Raja Singh <rajasgh18@gmail.com>
2024-09-26 03:41:02 +0530

    feat: authenticate function for checking authorisation

--- server/utils/security.py
+++ server/utils/security.py
@@ -33,3 +33,12 @@ def verify_token(token: str) -> dict:
         return payload['sub']
     except JWTError:
         raise HTTPException(403, detail="Unauthorized")
+
+# Helper function for authenticating users
+async def authenticate_user(request: Request):
+    token = request.cookies.get('access_token')
+    if(token is None):
+        raise HTTPException(status_code=403, detail="Unauthorized")
+    
+    user_id = verify_token(token)
+    request.state.user_id = user_id
