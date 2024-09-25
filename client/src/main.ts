08a4d27ec6ef52d667072f06da006416b699d1c7
Raja Singh <rajasgh18@gmail.com>
2024-09-26 03:42:17 +0530

    feat: added pinia store

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+
+app
+    .use(pinia)
+    .use(router)
+    .mount('#app')
