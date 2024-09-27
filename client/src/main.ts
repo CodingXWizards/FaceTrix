54ba374b3ccf768aac644ce13a9a5fd7dbcb4071
Raja Singh <rajasgh18@gmail.com>
2024-09-27 10:18:35 +0530

    some minor fixes and styles

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,10 +4,20 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 const app = createApp(App);
 const pinia = createPinia();
+const vuetify = createVuetify({
+    components,
+    directives,
+});
 
 app
     .use(pinia)
     .use(router)
+    .use(vuetify)
     .mount('#app')
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,37 +1,19 @@
 <script setup lang="ts">
-import { useAuthStore } from "@/store/authStore";
-import { useRouter } from "vue-router";
-
-const authStore = useAuthStore();
-const router = useRouter();
-
-async function handleLogout() {
-  try {
-    const response = await fetch("http://localhost:8000/api/auth/logout");
-    console.log(await response.json());
-    router.push("/login");
-  } catch (error) {
-    console.log(error);
-  }
-}
+import Navbar from '@/components/Navbar.vue';
 </script>
 
 <template>
-  <main>
-    <nav
-      class="w-full h-14 shadow-md shadow-gray-300 flex items-center justify-between px-10"
-    >
-      <div>
-        <h4>{{ authStore.user?.name }}</h4>
-        <p class="text-xs">{{ authStore.user?.designation }} (IPS)</p>
+  <main class="h-screen flex flex-col">
+    <Navbar />
+    <section class="flex-grow p-5 flex flex-col gap-y-5">
+      <div class="flex-grow flex flex-col justify-between bg-gray-200 rounded-lg">
+        <span></span>
+        <div>
+        </div>
       </div>
-      <button
-        class="p-2 px-4 bg-red-500 rounded-lg text-white text-sm"
-        @click="handleLogout"
-      >
-        Logout
-      </button>
-    </nav>
+      <RouterLink class="p-2 px-4 w-fit bg-orange-500 hover:bg-opacity-90 text-white rounded-lg" to="/camera">
+        Camera</RouterLink>
+    </section>
   </main>
 </template>
 
