b3c9b3595a76cbad964746ddedc33d2df602ed36
Raja Singh <rajasgh18@gmail.com>
2024-09-27 10:15:19 +0530

    feat: Navbar and Input component

---
+++ client/src/components/Input.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+interface Props {
+  id: string;
+  label: string;
+  value: string | number | null;
+  placeholder: string;
+}
+defineProps<Props>();
+
+const emit = defineEmits(["update:value"]);
+
+function handleChange(e: Event) {
+  const target = e.target as HTMLInputElement;
+  emit("update:value", target.value);
+}
+
+</script>
+
+<template>
+  <div class="w-full">
+    <label class="block" for="password">{{ label }}</label>
+    <input :id="id" type="text" :value="value" @change="handleChange" :placeholder="placeholder" />
+  </div>
+</template>
+
+<style scoped>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+input {
+  @apply w-full mt-1 p-2 border border-gray-400 rounded-lg focus:outline-none focus:border-orange-500
+}
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { useAuthStore } from "@/store/authStore";
+import { useRouter } from "vue-router";
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+async function handleLogout() {
+  try {
+    const response = await fetch("http://localhost:8000/api/auth/logout", {
+      method: "GET",
+      credentials: "include",
+    });
+    console.log(await response.json());
+    localStorage.removeItem("isAuthenticated");
+    router.push("/login");
+  } catch (error) {
+    console.log(error);
+  }
+}
+</script>
+
+<template>
+  <nav class="w-full h-14 shadow-md shadow-gray-300 flex items-center justify-between px-10">
+    <div class="flex items-center gap-x-3">
+      <img src="/police.png" alt="police" width="40" height="40">
+      <div>
+        <h5>{{ authStore.user?.name }}</h5>
+        <p class="text-xs">{{ authStore.user?.designation }}</p>
+      </div>
+    </div>
+    <button class="p-2 px-4 bg-red-600 rounded-lg text-white text-sm" @click="handleLogout">
+      Logout
+    </button>
+  </nav>
+</template>
+
+<style scoped></style>
